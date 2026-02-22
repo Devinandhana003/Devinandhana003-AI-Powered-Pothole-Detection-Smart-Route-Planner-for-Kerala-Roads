95497c820b84aeb4ba95e72d8a86a98faaeb878c
deepthavu001-ai <deepthavu001@gmail.com>
2026-02-22 11:50:57 +0530

    Update extract_potholes.py

--- extract_potholes.py
+++ extract_potholes.py
@@ -3,9 +3,9 @@ import csv
 import time
 
 # Load trained model
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO(r"D:\project\pothole_yolo\Data\runs\detect\train3\weights\best.pt")
 # Input image
-image_path = "test_image.jpg"
+image_path = r"D:\project\pothole_yolo\Data\train\images\pothole_1.jpg"
 
 # Run detection
 results = model(image_path)
@@ -17,11 +17,12 @@ with open("pothole_data.csv", mode="w", newline="") as file:
 
     for r in results:
         for box in r.boxes:
-            conf = float(box.conf)
+            conf = box.conf.item()
             x1, y1, x2, y2 = box.xyxy[0]
 
             # Calculate bounding box area
-            bbox_area = (x2 - x1) * (y2 - y1)
+            x1, y1, x2, y2 = box.xyxy[0]
+            bbox_area = (x2.item() - x1.item()) * (y2.item() - y1.item())
 
             # Severity calculation
             severity = min(10, round((bbox_area * conf) / 10000, 2))
